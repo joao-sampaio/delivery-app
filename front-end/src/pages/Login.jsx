53ac8111bd068b1cfe3fb12c4a54c49efc7c0b36
Maysa <maybsalvalaio@gmail.com>
2022-11-29 20:45:59 -0300

    feat: iniciando pagina de login

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import { loginSubmit } from '../service/requests';
+
+function Login() {
+  const [invalid, setInvalid] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const validateEmailAndSenha = () => {
+    const regex = /\S+@\S+\.\S+/i;
+    const MIN_LENGTH = 6;
+    if (regex.test(email) && senha.length >= MIN_LENGTH) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+
+  const changeEmail = (value) => {
+    setEmail(value);
+    validateEmailAndSenha();
+  };
+
+  const changeSenha = (value) => {
+    setSenha(value);
+    validateEmailAndSenha();
+  };
+
+  const handleSubmit = async () => {
+    const body = { email, password: senha };
+    try {
+      const result = await loginSubmit(body);
+      console.log(result);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <form>
+      <input
+        data-testid="common_login__input-email"
+        type="email"
+        placeholder="email"
+        onChange={ (event) => changeEmail(event.target.value) }
+        value={ email }
+      />
+      <input
+        data-testid="common_login__input-password"
+        type="password"
+        placeholder="password"
+        onChange={ (event) => changeSenha(event.target.value) }
+        value={ senha }
+      />
+      <button
+        type="button"
+        onClick={ handleSubmit }
+        disabled={ isDisabled }
+        data-testid="common_login__button-login"
+      >
+        Login
+      </button>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+      </button>
+      {
+        invalid && (
+          <div data-testid="common_login__element-invalid-email">
+            Email ou senha inválidos
+          </div>
+        )
+      }
+    </form>
+  );
+}
+
+export default Login;
---
+++ front-end/src/service/requests.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+export const loginSubmit = async (body) => {
+  const result = await api.post('/login', body);
+  return result;
+};
