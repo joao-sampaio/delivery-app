1e33abdd7c091221b59e78d66b2f807753ffbd88
joao-sampaio <joaoantoniosampaio04@outlook.com>
2022-11-30 10:25:14 -0300

    refatoração do formulario

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,29 +2,37 @@ import { useState } from 'react';
 import { loginSubmit } from '../service/requests';
 
 function Login() {
-  const [invalid, setInvalid] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(true);
+  // const [invalid, setInvalid] = useState(false);
+  // const [isDisabled, setIsDisabled] = useState(true);
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
-  const validateEmailAndSenha = () => {
+  // const validateEmailAndSenha = () => {
+  //   const regex = /\S+@\S+\.\S+/i;
+  //   const MIN_LENGTH = 6;
+  //   if (regex.test(email) && senha.length >= MIN_LENGTH) {
+  //     setIsDisabled(false);
+  //   } else {
+  //     setIsDisabled(true);
+  //   }
+  //   console.log(senha)
+  //   setIsDisabled(!(regex.test(email) && senha.length >= MIN_LENGTH))
+  // };
+
+  const isDisabled = () => {
     const regex = /\S+@\S+\.\S+/i;
     const MIN_LENGTH = 6;
-    if (regex.test(email) && senha.length >= MIN_LENGTH) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
-    }
+    return !(regex.test(email) && senha.length >= MIN_LENGTH);
   };
 
   const changeEmail = (value) => {
     setEmail(value);
-    validateEmailAndSenha();
+    // validateEmailAndSenha();
   };
 
   const changeSenha = (value) => {
     setSenha(value);
-    validateEmailAndSenha();
+    // validateEmailAndSenha();
   };
 
   const handleSubmit = async () => {
@@ -43,20 +51,20 @@ function Login() {
         data-testid="common_login__input-email"
         type="email"
         placeholder="email"
-        onChange={ (event) => changeEmail(event.target.value) }
         value={ email }
+        onChange={ (event) => changeEmail(event.target.value) }
       />
       <input
         data-testid="common_login__input-password"
         type="password"
         placeholder="password"
-        onChange={ (event) => changeSenha(event.target.value) }
         value={ senha }
+        onChange={ (event) => changeSenha(event.target.value) }
       />
       <button
         type="button"
         onClick={ handleSubmit }
-        disabled={ isDisabled }
+        disabled={ isDisabled() }
         data-testid="common_login__button-login"
       >
         Login
