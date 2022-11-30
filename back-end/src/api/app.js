553b3a39f401c854929998c1904b3f9cbee2b91a
Maysa <maybsalvalaio@gmail.com>
2022-11-30 15:43:52 -0300

    fix: eslint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const { userRouter } = require('../database/routers');
-const bodyParser = require('body-parser');
-
 
 const app = express();
 app.use(cors());
-app.use(express.json())
-app.use('/users', userRouter)
+app.use(express.json());
+app.use('/users', userRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
