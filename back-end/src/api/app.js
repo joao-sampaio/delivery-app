444d0f14b34d25c77dbd8d3b736c9126a060daf0
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-06-30 18:59:27 -0300

    cors headers setup

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -9,7 +9,7 @@ const corsOptions = {
     optionsSuccessStatus: 200
   }
 
-app.use(cors(corsOptions));
+app.use('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public'));
 
