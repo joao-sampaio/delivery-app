f449f1d88d3d7c8234c9dbf038aedd90aeb10b85
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-06-30 13:11:06 -0300

    back-end deploy setup

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const { userRouter, productRouter, salesRouter } = require('../database/routers');
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://placeholder.com',
+    optionsSuccessStatus: 200
+  }
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
