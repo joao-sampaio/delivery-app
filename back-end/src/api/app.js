4b6bb702d8df528a587b939f8c0f23a282bb7fd1
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-06-30 19:19:25 -0300

    cors update

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,11 +6,17 @@ const app = express();
 
 const corsOptions = {
     origin: 'https://drinkdelivery.vercel.app',
-    allowedHeaders:  'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:  '*',
     optionsSuccessStatus: 200
   }
 
 app.use('*', cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(express.static('public'));
 
