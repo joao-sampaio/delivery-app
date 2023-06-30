9ebc1301996f714d3cabbb33c1af4efd5e6ea846
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-06-30 19:53:05 -0300

    cors update

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,12 +12,6 @@ const corsOptions = {
   }
 
 app.use('*', cors(corsOptions));
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://drinkdelivery.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
 app.use(express.json());
 app.use(express.static('public'));
 
