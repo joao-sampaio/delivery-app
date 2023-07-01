b7dbd7358b5e6b88cfb0510956b7e583727e61f3
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-07-01 00:00:35 -0300

    database config.js

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -10,11 +10,13 @@ const suffix = {
   test: "-test",
 };
 
+const databaseName = environment === 'production' ?
+process.env.MYSQL_DB_NAME : `${suffix[environment] || suffix.test}`
+
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  database: databaseName,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
